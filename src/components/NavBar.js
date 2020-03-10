c799bb394d3b8f0d2cc9a8b208830c07ede9f577
github username <github email address>
2020-03-10 13:41:34 -0400

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,11 +2,26 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
-  return (
-    <div>
-      {/*{code here}*/}
-    </div>
-  );
-};
+    return (
+      <div className='navbar'>
+        <NavLink
+          to="/"
+          exact
+        >Home</NavLink>
+        <NavLink
+          to="/directors"
+          exact
+        >Directors</NavLink>
+        <NavLink
+          to="/actors"
+          exact
+        >Actors</NavLink>
+        <NavLink
+          to="/movies"
+          exact
+        >Movies</NavLink>
+      </div>
+    )
+}
 
 export default NavBar;
